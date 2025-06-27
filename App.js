3f85606889c27ca65926340f6acac1d9c364af9d
Sebastian Sippl <sebastiansippl@Mac.lan>
2025-06-27 15:40:43 +0200

    add minimal example

--- App.js
+++ App.js
@@ -1,9 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { createAudioPlayer, setAudioModeAsync } from "expo-audio";
+import { useEffect } from "react";
+
+const breakEnd = require("./break-end.mp3");
+
+let player;
+
+async function stopSound() {
+  if (player) {
+    player.seekTo(0);
+    player.pause();
+  }
+}
+
+function playSound() {
+  console.log("Playing");
+  try {
+    if (!player) {
+      player = createAudioPlayer(breakEnd);
+      player.volume = 1;
+      player.addListener("playbackStatusUpdate", async (audio) => {
+        if (audio.didJustFinish) {
+          console.log("Sound finished.");
+          stopSound();
+        }
+      });
+    } else if (player) {
+      stopSound();
+    }
+
+    if (player) {
+      player.seekTo(0);
+      player.play();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default function App() {
+  useEffect(() => {
+    const initSound = async () => {
+      console.log("Setting up Android Audio Mode");
+      try {
+        await setAudioModeAsync({
+          playsInSilentMode: true,
+          interruptionModeAndroid: "duckOthers",
+          shouldPlayInBackground: false,
+          interruptionMode: "duckOthers",
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      console.log("Audio Set-Up complete");
+    };
+    initSound();
+  }, []);
+
   return (
     <View style={styles.container}>
+      <Button title="Test Audio" onPress={() => playSound()}></Button>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +71,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
